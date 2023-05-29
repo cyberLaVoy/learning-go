5c0beaad94d71a0ab334ce5bf567a921aaeaf0a6
LaVoy <brentenlavoy@gmail.com>
2023-05-29 15:35:27 -0600

    Tabs changed to 4 spaces for easier reading on GitHub.

--- classify/classify.go
+++ classify/classify.go
@@ -1,138 +1,138 @@
 package main
 
 import ( 
-	"fmt"
-	"time"
-	"math"
-	"strconv"
+    "fmt"
+    "time"
+    "math"
+    "strconv"
 )
 
 func fanIn(input1, input2, input3 <-chan string) <-chan string {
-	c := make(chan string)
-	timeout := time.After(10 * time.Second)
-	go func() {
-		for {
-			select {
-			case s := <-input1: c<- s
-			case s := <-input2: c<- s
-			case s := <-input3: c<- s
-			case <-timeout:
-				fmt.Println("Fan in classifications timeout.")
-				return 
-			}
-		}
-	}()
-	return c
+    c := make(chan string)
+    timeout := time.After(10 * time.Second)
+    go func() {
+        for {
+            select {
+            case s := <-input1: c<- s
+            case s := <-input2: c<- s
+            case s := <-input3: c<- s
+            case <-timeout:
+                fmt.Println("Fan in classifications timeout.")
+                return 
+            }
+        }
+    }()
+    return c
 }
 
 func isPrime(n int) <-chan string {
-	c := make(chan string)
-	go func() {
-		var is_prime = true
-		if n <= 2 || n % 2 == 0 {
-			is_prime = false
-		} else {
-			max := int( math.Sqrt( float64(n) ) )
-			for i := 3; i <= max ; i += 2 {
-				if n % i == 0 {
-					is_prime = false
-					break
-				}
-			}
-		}
-		c <- "Is prime #? " + strconv.FormatBool(is_prime)
-	}()
-	return c
+    c := make(chan string)
+    go func() {
+        var is_prime = true
+        if n <= 2 || n % 2 == 0 {
+            is_prime = false
+        } else {
+            max := int( math.Sqrt( float64(n) ) )
+            for i := 3; i <= max ; i += 2 {
+                if n % i == 0 {
+                    is_prime = false
+                    break
+                }
+            }
+        }
+        c <- "Is prime #? " + strconv.FormatBool(is_prime)
+    }()
+    return c
 }
 
 func isPerfectSquare(n int) bool {
-	s := int( math.Sqrt(float64(n)) )
-	return s*s == n
+    s := int( math.Sqrt(float64(n)) )
+    return s*s == n
 }
 func isFibonacci(n int) <-chan string {
-	c := make(chan string)
-	go func() {
-		is_fibonacci := isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4)
-		c <- "Is Fibonacci #? " + strconv.FormatBool(is_fibonacci)	       
-	}()
-	return c
+    c := make(chan string)
+    go func() {
+        is_fibonacci := isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n - 4)
+        c <- "Is Fibonacci #? " + strconv.FormatBool(is_fibonacci)           
+    }()
+    return c
 }
 
 func collatzDistance(n int) <-chan string {
-	c := make(chan string)
-	go func(){
-		step := 1
-		for n != 1 {
-			if n % 2 == 0 {
-				n /= 2
-			} else {
-				n *= 3
-				n++
-			}
-			step++
-		}
-		c <- "Collatz Distance: " + strconv.Itoa(step)
-	}()
-	return c
+    c := make(chan string)
+    go func(){
+        step := 1
+        for n != 1 {
+            if n % 2 == 0 {
+                n /= 2
+            } else {
+                n *= 3
+                n++
+            }
+            step++
+        }
+        c <- "Collatz Distance: " + strconv.Itoa(step)
+    }()
+    return c
 }
 
 func numberSuffix(n int) string {
-	v := "th"
-	if n == 1 {
-		v = "st"
-	} else if n == 2 {
-		v = "nd"
-	} else if n == 3 {
-		v = "rd"
-	} 
-	return v
+    v := "th"
+    if n == 1 {
+        v = "st"
+    } else if n == 2 {
+        v = "nd"
+    } else if n == 3 {
+        v = "rd"
+    } 
+    return v
 }
 func calcPolygon(s int, n int, c chan<- string) {
-	go func() {
-		m := 1
-		for {
-			// candidate is the mth s-sided polygonal number
-			// candidate := (m*m*(s-2)-m*(s-4))/2
-			candidate := (s-2)*(m*(m+1)/2)-(s-3)*m
-			if n == candidate {
-				c<- "Is the " + strconv.Itoa(m) + numberSuffix(m) + " " + strconv.Itoa(s) + "-sided regular polygon."
-			}
-			if n <= candidate {
-				return
-			}
-			m++
-		}
-	}()
+    go func() {
+        m := 1
+        for {
+            // candidate is the mth s-sided polygonal number
+            // candidate := (m*m*(s-2)-m*(s-4))/2
+            candidate := (s-2)*(m*(m+1)/2)-(s-3)*m
+            if n == candidate {
+                c<- "Is the " + strconv.Itoa(m) + numberSuffix(m) + " " + strconv.Itoa(s) + "-sided regular polygon."
+            }
+            if n <= candidate {
+                return
+            }
+            m++
+        }
+    }()
 }
 func whichPolygons(n int) {
-	c := make(chan string)
-	timeout := time.After(20 * time.Second)
-	go func() {
-		s := 3
-		for s <= n {
-			calcPolygon(s, n, c)
-			s++
-		}
-	}()
-	for {
-		select {
-		case s := <-c:
-			fmt.Println(s)
-		case <-timeout:
-			fmt.Println("Polygon classification timeout.")
-			return
-		}
-	}
+    c := make(chan string)
+    timeout := time.After(20 * time.Second)
+    go func() {
+        s := 3
+        for s <= n {
+            calcPolygon(s, n, c)
+            s++
+        }
+    }()
+    for {
+        select {
+        case s := <-c:
+            fmt.Println(s)
+        case <-timeout:
+            fmt.Println("Polygon classification timeout.")
+            return
+        }
+    }
 }
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	c := fanIn(isPrime(n), isFibonacci(n), collatzDistance(n))
-	go func() {
-		for i := 0; i < 3; i++ {
-			fmt.Println(<-c)
-		}
-	}()
-	whichPolygons(n)
+    var n int
+    fmt.Scan(&n)
+    c := fanIn(isPrime(n), isFibonacci(n), collatzDistance(n))
+    go func() {
+        for i := 0; i < 3; i++ {
+            fmt.Println(<-c)
+        }
+    }()
+    whichPolygons(n)
 }
--- scratch/scratch.go
+++ scratch/scratch.go
@@ -1,72 +1,82 @@
 package main
 
-import ( 
-	"fmt"
-	"time"
-	"math/rand"
+import (
+    "fmt"
+    "math/rand"
+    "time"
 )
 
 func fanInOrd(input1, input2 <-chan string) <-chan string {
-	c := make(chan string)
-	go func() { for { c<- <-input1 } }()
-	go func() { for { c<- <-input2 } }()
-	return c
+    c := make(chan string)
+    go func() {
+        for {
+            c <- <-input1
+        }
+    }()
+    go func() {
+        for {
+            c <- <-input2
+        }
+    }()
+    return c
 }
 
 func fanInSel(input1, input2 <-chan string) <-chan string {
-	c := make(chan string)
-	go func() {
-		for {
-			select {
-			case s := <-input1: c<- s
-			case s := <-input2: c<- s
-			}
-		}
-	}()
-	return c
+    c := make(chan string)
+    go func() {
+        for {
+            select {
+            case s := <-input1:
+                c <- s
+            case s := <-input2:
+                c <- s
+            }
+        }
+    }()
+    return c
 }
 
 func boring(message string) <-chan string {
-	c := make(chan string)
-	go func() {
-		for {
-			c <- message
-			time.Sleep(time.Duration(rand.Intn(1e4))*time.Millisecond)
-		}
-	}()
-	return c
+    c := make(chan string)
+    go func() {
+        for {
+            c <- message
+            time.Sleep(time.Duration(rand.Intn(1e4)) * time.Millisecond)
+        }
+    }()
+    return c
 }
 
 func boringWithQuit(message string, quit chan string) <-chan string {
-	c := make(chan string)
-	go func() {
-		for {
-			select {
-			case c<- message: 
-				time.Sleep(time.Duration(rand.Intn(1e3))*time.Millisecond)
-			case s := <-quit:
-				//clean up
-				fmt.Println(s)
-				quit <- "See yah!"
-				return
-			}
-		}
-	}()
-	return c
+    c := make(chan string)
+    go func() {
+        for {
+            select {
+            case c <- message:
+                time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+            case s := <-quit:
+                //clean up
+                fmt.Println(s)
+                quit <- "See yah!"
+                return
+            }
+        }
+    }()
+    return c
 }
 
 func main() {
-	quit := make(chan string)
-	c := boringWithQuit("Give me the order to stop...", quit)
-	for i := 0; i < 10; i++{
-		fmt.Println(<-c)
-	}
-	quit<- "Okay, stop."
-	fmt.Println(<-quit)
+    quit := make(chan string)
+    c := boringWithQuit("Give me the order to stop...", quit)
+    for i := 0; i < 10; i++ {
+        fmt.Println(<-c)
+    }
+    quit <- "Okay, stop."
+    fmt.Println(<-quit)
 
-	//c := fanInOrd(boring("Boring 1..."), boring("Boring 2..."))
-	ca := fanInSel(boring("Boring 1..."), boring("Boring 2..."))
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-ca)
-	}
+    //c := fanInOrd(boring("Boring 1..."), boring("Boring 2..."))
+    ca := fanInSel(boring("Boring 1..."), boring("Boring 2..."))
+    for i := 0; i < 10; i++ {
+        fmt.Println(<-ca)
+    }
 }
